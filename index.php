64c28bff1b39e47e89bf182779d7c1b287ccd82b
mburhanpur <mburhanpur6@gmail.com>
2025-10-29 19:15:28 +0530

    index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<section id="projects">
+  <h2>My Projects</h2>
+  <div class="project-list">
+    <div class="project">
+      <h3>Personal Portfolio</h3>
+      <p>A responsive portfolio website built using HTML, CSS, and JavaScript.</p>
+      <a href="https://github.com/mburhanpur/my-portfolio" target="_blank">View Code</a>
+    </div>
+
+    <div class="project">
+      <h3>Online Clothing Store</h3>
+      <p>WordPress + WooCommerce theme developed for an eCommerce business.</p>
+      <a href="https://github.com/mburhanpur/online-clothing-store" target="_blank">View Code</a>
+    </div>
+
+    <div class="project">
+      <h3>Fitness Tracker App</h3>
+      <p>A mobile-friendly web app to track workouts, built using Bootstrap and JS.</p>
+      <a href="https://github.com/mburhanpur/fitness-tracker" target="_blank">View Code</a>
+    </div>
+  </div>
+</section>
